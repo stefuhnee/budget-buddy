21653e6394ad7536e50d30f8ae7a0f1a973a1bd8
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-04 15:54:45 -0700

    User input correctly updating the expense property on each budget object.

--- js/budget.js
+++ js/budget.js
@@ -10,18 +10,26 @@ function collectBudgetData(event){
 
   var enterIncome = parseInt(event.target.enterIncome.value);
   console.log(enterIncome);
-  var rentMortgage = parseInt(event.target.rentMortgage.value);
-  console.log(rentMortgage);
-  var foodGroceries = parseInt(event.target.foodGroceries.value);
-  console.log(foodGroceries);
-  var insurance = parseInt(event.target.insurance.value);
-  console.log(insurance);
-  var utilities = parseInt(event.target.utilities.value);
-  console.log(utilities);
-  var loansCcDebt = parseInt(event.target.loansCcDebt.value);
-  console.log(loansCcDebt);
-  var transportation = parseInt(event.target.transportation.value);
-  console.log(transportation);
+  rentExpense.expense = parseInt(event.target.rentMortgage.value);
+  console.log(rentExpense);
+  foodExpense.expense = parseInt(event.target.foodGroceries.value);
+  console.log(foodExpense);
+  insuranceExpense.expense = parseInt(event.target.insurance.value);
+  console.log(insuranceExpense);
+  utilitiesExpense.expense = parseInt(event.target.utilities.value);
+  console.log(utilitiesExpense);
+  loansExpense.expense = parseInt(event.target.loansCcDebt.value);
+  console.log(loansExpense);
+  transportationExpense.expense = parseInt(event.target.transportation.value);
+  console.log(transportationExpense);
 }
+
+// Budget expense objects
+var rentExpense = new budgetExpense('rent', 0);
+var foodExpense = new budgetExpense('food', 0);
+var insuranceExpense = new budgetExpense('insurance', 0);
+var utilitiesExpense = new budgetExpense('utilities', 0);
+var loansExpense = new budgetExpense('loans', 0);
+var transportationExpense = new budgetExpense('transportation', 0);
 
 form.addEventListener('submit', collectBudgetData);
