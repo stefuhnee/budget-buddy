8376dbe9649d8e8bcde4ddf56dd5c84587dac619
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-04-05 08:33:14 -0700

    initial pie chart

--- js/budget.js
+++ js/budget.js
@@ -27,3 +27,27 @@ function collectBudgetData(event){
 
 // event listener
 form.addEventListener('submit', collectBudgetData);
+
+// pie chart info below
+var myPieChart = new Chart(ctx[0]).Pie(data,options);
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
