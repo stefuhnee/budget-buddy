07dc7096e2bc7c35c2c782ad8d75c53f01672364
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-05 08:34:10 -0700

    Calculated savings remaining within the event handler in savings.js

--- js/savings.js
+++ js/savings.js
@@ -1,15 +1,26 @@
+var howMuch;
+var addMoney;
+var savingsArray = [];
+var savingsRemaining;
+
 console.log('javascript is working dummy');
 
 var savingsForm = document.getElementById('savingsForm');
+
+// Collects user info from savings form (event hnalder)
 function collectSavingsData(event){
   event.preventDefault();
 
-  var savingsFor = parseInt(event.target.savingsFor.value);
+  var savingsFor = event.target.savingsFor.value;
   console.log(savingsFor);
   var howMuch = parseInt(event.target.howMuch.value);
   console.log(howMuch);
   var addMoney = parseInt(event.target.addMoney.value);
   console.log(addMoney);
+  var savingsRemaining = (howMuch - addMoney);
+  console.log(savingsRemaining);
+  return savingsRemaining;
 }
 
+// Event listener
 savingsForm.addEventListener('submit', collectSavingsData);
