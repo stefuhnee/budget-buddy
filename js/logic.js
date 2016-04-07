b72a090775bb00f92c4cd750b239724991674fd9
vbenavente <vbenavente@hotmail.com>
2016-04-07 11:35:46 -0700

    progress bar resets when starting new goal

--- js/logic.js
+++ js/logic.js
@@ -5,6 +5,7 @@ function clearLocalStorage() {
   form.reset();
   var section = document.getElementById('savingsData');
   section.removeChild(section.childNodes[0]);
+  updateProgress(0);
 }
 
 // uses inputted monthly income value to calculate remaining income after budgeting
@@ -143,6 +144,8 @@ function collectSavingsData(event){
 // Progress bar
   if (currentSavings <= howMuch) {
     updateProgress((currentSavings / howMuch) * 100);
+  }else if (currentSavings >= howMuch) {
+    updateProgress (100);
   };
 }
 
